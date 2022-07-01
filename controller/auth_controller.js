ce842238d9b665a71eb9966989ac6d864636236e
Jassi50 <87673773+Jassi50@users.noreply.github.com>
2022-06-30 18:09:32 -0700

    Add files via upload

---
+++ controller/auth_controller.js
@@ -0,0 +1,23 @@
+let database = require("../database");
+
+let authController = {
+  login: (req, res) => {
+    res.render("auth/login");
+  },
+
+  register: (req, res) => {
+    res.render("auth/register");
+  },
+
+  loginSubmit: (req, res) => {
+    // implement
+    res.render("dashboard");
+  },
+
+  registerSubmit: (req, res) => {
+    // implement
+    res.render("auth/login");
+  },
+};
+
+module.exports = authController;
---
+++ controller/reminder_controller.js
@@ -0,0 +1,62 @@
+let database = require("../database");
+
+let remindersController = {
+  list: (req, res) => {
+    res.render("reminder/index", { reminders: database.cindy.reminders });
+  },
+
+  new: (req, res) => {
+    res.render("reminder/create");
+  },
+
+  listOne: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    if (searchResult != undefined) {
+      res.render("reminder/single-reminder", { reminderItem: searchResult });
+    } else {
+      res.render("reminder/index", { reminders: database.cindy.reminders });
+    }
+  },
+
+  create: (req, res) => {
+    let reminder = {
+      id: database.cindy.reminders.length + 1,
+      title: req.body.title,
+      description: req.body.description,
+      completed: false,
+    };
+    database.cindy.reminders.push(reminder);
+    res.redirect("/reminders");
+  },
+
+  edit: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    res.render("reminder/edit", { reminderItem: searchResult });
+  },
+
+  update: (req, res) => {
+
+    //title, descipriton, completed same as create, how to find id? keep id same? How to change completed value?
+    // implement this code
+    // let reminder = {
+    // title: req.body.title,
+    // description: req.body.description,
+    // completed: false,
+    // };
+  },
+
+  delete: (req, res) => {
+    //find id, and delete from database array?
+    // Implement this code
+    // let reminderToFind = req.params.id;
+    // let filtered = Database.filter(data => data != reminderToFind)
+  },
+};
+
+module.exports = remindersController;
